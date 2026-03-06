76ff37e5136680757377edb138bb90d1b7c5e4ff
cherradiothmane <cherradiothmane.snrt@gmail.com>
2026-03-06 23:30:34 +0000

    fix: update imports ChapterSummary -> TopicSummary

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,19 +1,11 @@
 from .pipeline import VideoPipeline, PipelineConfig, PipelineResult
 from .transcriber import Transcriber, TranscriptionResult, TranscriptionSegment
-from .summarizer import Summarizer, SummaryResult, ChapterSummary
+from .summarizer import Summarizer, SummaryResult, TopicSummary
 from .ocr_extractor import OCRExtractor, OCRResult, OCREntry
 
 __all__ = [
-    "VideoPipeline",
-    "PipelineConfig",
-    "PipelineResult",
-    "Transcriber",
-    "TranscriptionResult",
-    "TranscriptionSegment",
-    "Summarizer",
-    "SummaryResult",
-    "ChapterSummary",
-    "OCRExtractor",
-    "OCRResult",
-    "OCREntry",
+    "VideoPipeline", "PipelineConfig", "PipelineResult",
+    "Transcriber", "TranscriptionResult", "TranscriptionSegment",
+    "Summarizer", "SummaryResult", "TopicSummary",
+    "OCRExtractor", "OCRResult", "OCREntry",
 ]
